ceab81d825821d4ecce04214a73e899d0a2107d0
arups330 <65585501+arups330@users.noreply.github.com>
2021-07-16 21:04:28 +0600

    Add files via upload

---
+++ Counter/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Counters from './component/counters';
+import Counter from './component/counter';
+
+ReactDOM.render(<Counters />, document.getElementById('root'));
+
+
+reportWebVitals();
